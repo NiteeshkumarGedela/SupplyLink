9e6dbc20fc22655072a22fd7cb89895f79bc0b59
username <wecp-dummy@gmail.com>
2024-12-28 13:29:16 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJpa.java
@@ -1,5 +1,89 @@
 package com.wecp.progressive.service.impl;
 
-public class WarehouseServiceImplJpa  {
+import java.util.List;
+
+import com.wecp.progressive.entity.Warehouse;
+import com.wecp.progressive.service.WarehouseService;
+
+public class WarehouseServiceImplJpa implements WarehouseService  {
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    @Override
+    public int addWarehouse(Warehouse warehouse) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void deleteWarehouse(int warehouseId) {
+        // TODO Auto-generated method stub
+        WarehouseService.super.deleteWarehouse(warehouseId);
+    }
+
+    @Override
+    public void emptyArrayList() {
+        // TODO Auto-generated method stub
+        WarehouseService.super.emptyArrayList();
+    }
+
+    @Override
+    public List<Warehouse> getAllWarehouses() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Warehouse getWarehouseById(int warehouseId) {
+        // TODO Auto-generated method stub
+        return WarehouseService.super.getWarehouseById(warehouseId);
+    }
+
+    @Override
+    public List<Warehouse> getWarehouseBySupplier(int supplierId) {
+        // TODO Auto-generated method stub
+        return WarehouseService.super.getWarehouseBySupplier(supplierId);
+    }
+
+    @Override
+    public List<Warehouse> getWarehousesSortedByCapacity() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateWarehouse(Warehouse warehouse) {
+        // TODO Auto-generated method stub
+        WarehouseService.super.updateWarehouse(warehouse);
+    }
+
 
 }
